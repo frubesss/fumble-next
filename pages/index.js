3eb0a44ee46cfb173dd749f0b934c33a8f59b156
Craig Robertson <robertson.cra@gmail.com>
2020-06-12 08:53:27 +0100

    add font files

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,15 @@ export default function App() {
   return (
     <>
       <Head>
+        <link
+          rel="stylesheet"
+          href="https://d3tbpaf5tfzpa.cloudfront.net/buenos-aires.css"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://d3tbpaf5tfzpa.cloudfront.net/source-sans-pro.css"
+        />
         <title>Fumble</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
